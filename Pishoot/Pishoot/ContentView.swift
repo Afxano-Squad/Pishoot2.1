1dd761476c67ae2926252a061b85e8b15c728074
YurikoAishinselo123 <aishinseloyuriko124@gmail.com>
2024-10-23 19:32:42 +0700

    Initial Commit

---
+++ Pishoot/Pishoot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pishoot
+//
+//  Created by Yuriko AIshinselo on 23/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pishoot/Pishoot/PishootApp.swift
@@ -0,0 +1,17 @@
+//
+//  PishootApp.swift
+//  Pishoot
+//
+//  Created by Yuriko AIshinselo on 23/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PishootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
